e3a0b503499bfe14374e784853f9beeeb9b63ada
Adam <adam.wolanski94@gmail.com>
2017-04-13 18:43:58 +0200

    unity-workshop #1

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject BoxPrefabGameObject;
+    [SerializeField]
+    private GameObject _altBoxPrefabGameObject;
+
+    void Awake()
+    {
+        
+    }
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _altBoxPrefabGameObject = Resources.Load("Box") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("Jump"))
+	    {
+	        Instantiate(_altBoxPrefabGameObject, this.transform.position, Quaternion.identity);
+	    }
+	}
+}

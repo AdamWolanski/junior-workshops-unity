6ba6e468208aef124eaf2337566d168cfd79ac1c
Adam <adam.wolanski94@gmail.com>
2017-04-20 17:58:16 +0200

    trigger update

---
+++ Assets/Scripts/DebugCollider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCollider : MonoBehaviour
+{
+
+    private BoxCollider2D _boxCollider2D;
+
+    private void Awake()
+    {
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "box")
+            Debug.Log("kolizja");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("trigger");
+    }
+}
